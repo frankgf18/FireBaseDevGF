a70cc2b5571fed4012513b97ee83b6e021cfd28a
frankgf18 <frankandersongf.2001@gmail.com>
2022-06-08 01:01:43 -0500

    BASE DE DATOS REMOTO

--- app/src/main/java/com/example/firebasedevgf/MainActivity.kt
+++ app/src/main/java/com/example/firebasedevgf/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.firebasedevgf
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.firebasedevgf.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dataBase = Firebase.database.reference
+
+        val listener = object : ValueEventListener{
+            @SuppressLint("SetTextI18n")
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val data = snapshot.getValue(String::class.java)
+                binding.nombre.text = "Firebase Modo remoto: $data"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        }
+
+        val dataRef = dataBase.child("saludo_firebase").child("data")
+
+        dataRef.addValueEventListener(listener)
+
+
+        val llamada = object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val data = snapshot.getValue(String::class.java)
+                binding.nombre.text = "FireB"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
     }
 }
